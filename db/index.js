2b65b8262111df09b1dd6ad4f5905f5f27781190
jazminejose <jazzi.lyn217@gmail.com>
2021-12-14 14:30:55 -0800

    code

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2')
 
-const db = mysql.createConnection('mysql://root:rootroot@localhost:3000/to_do_list_db')
+const db = mysql.createConnection('mysql://root:rootroot@localhost:3306/to_do_list_db')
 
 module.exports = db
